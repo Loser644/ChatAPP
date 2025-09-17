ad2477cc0b44268a8d4c6fba34de0d957bbef96f
TheCoding Freak <mshooda644@gmail.com>
2025-09-17 14:34:47 +0530

    call first api

--- server/nodeServer/Controllers/myConnectionFile.js
+++ server/nodeServer/Controllers/myConnectionFile.js
@@ -12,7 +12,7 @@ async function setConnection() {
             waitForConnections:true,
             connectionLimit:process.env.mySQL_limit
         })
-        console.log(chalk.green("Succesfully connected with mySQL"))
+        console.log(chalk.green.bold("Succesfully build connection with MySQL"))
         return connection;
     } catch (error) {
         console.log(chalk.red("something went wrong"+error.message))
--- server/nodeServer/myServer.js
+++ server/nodeServer/myServer.js
@@ -1,7 +1,18 @@
 import express from 'express';
 import chalk from 'chalk';
+import {connection} from './Controllers/myConnectionFile.js';
 let myApp = express();
-let port = 3222
+let port = 3222;
+myApp.use(express.json())
+myApp.get("/test",async (rkv,rspo) => {
+    try {
+        let [rows] = await connection.query("SELECT * FROM USERS;")
+        rspo.status(201).send({rows});
+    } catch (error) {
+        rspo.status(500).send({msg:"Something went wrong",details:error.message});
+    }
+})
+
 myApp.listen(port,()=>{
-    console.log(chalk.greenBright.white.italic.bold("server is start on "+port))
+    console.log(chalk.greenBright.yellow.italic.bold("server is start on "+port))
 })
